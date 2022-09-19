7f8fd17100532d0bcadf1be3deddfc4ed54d0165
Zero-imagination <disagreed7@gmail.com>
2022-09-19 23:44:32 +0300

    added descriptions of methods for interaction between services and the DAO layer

--- app/src/main/java/com/edu/ulab/app/service/impl/UserServiceImpl.java
+++ app/src/main/java/com/edu/ulab/app/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.edu.ulab.app.service.impl;
 
+import com.edu.ulab.app.dao.impl.UserDaoImpl;
 import com.edu.ulab.app.dto.UserDto;
+import com.edu.ulab.app.entity.User;
 import com.edu.ulab.app.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,12 +10,18 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
+    private final UserDaoImpl userDao = new UserDaoImpl();
     @Override
     public UserDto createUser(UserDto userDto) {
+        User user = new User();
+        user.setAge(userDto.getAge());
+        user.setFullName(userDto.getFullName());
+        user.setTitle(userDto.getTitle());
+        user.setListBookId(userDto.getListBooksId());
         // сгенерировать идентификатор
         // создать пользователя
         // вернуть сохраненного пользователя со всеми необходимыми полями id
-        userDto.setId(1L);
+        userDto.setId(userDao.create(user).getId());
         return userDto;
     }
 
@@ -23,12 +31,18 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDto getUserById(Long id) {
-        return null;
+    public UserDto getUserById(Long userId) {
+        User user = userDao.getUserById(userId);
+        UserDto userDto=new UserDto();
+        userDto.setAge(user.getAge());
+        userDto.setFullName(user.getFullName());
+        userDto.setTitle(user.getTitle());
+        userDto.setListBooksId(user.getListBookId());
+        return userDto;
     }
 
     @Override
-    public void deleteUserById(Long id) {
-
+    public void deleteUserById(Long userId) {
+        userDao.deleteUserById(userId);
     }
 }
