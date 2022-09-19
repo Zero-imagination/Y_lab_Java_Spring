05c652dac063ad8202f54c8f1e178c0bac7302cb
Zero-imagination <disagreed7@gmail.com>
2022-09-19 23:45:24 +0300

    added field listBookId

--- app/src/main/java/com/edu/ulab/app/web/request/UserRequest.java
+++ app/src/main/java/com/edu/ulab/app/web/request/UserRequest.java
@@ -2,9 +2,12 @@ package com.edu.ulab.app.web.request;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class UserRequest {
     private String fullName;
     private String title;
     private int age;
+    private List<Long> listBookId;
 }
